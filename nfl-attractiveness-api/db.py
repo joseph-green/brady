38ad727270830dac1a2ebc1a7869b6d50130b796
joseph-green <green.joseph.school@gmail.com>
2018-07-09 21:06:03 -0400

    refactored json response

--- nfl-attractiveness-api/db.py
+++ nfl-attractiveness-api/db.py
@@ -1,4 +1,5 @@
 import sqlite3
+import json
 
 def db_connect():
     db_conn = sqlite3.connect('./nfl.db')
@@ -19,9 +20,14 @@ def get_player_by_id(player_id):
 
     player = db.fetchone()
 
-    return player
+    keys = [des[0] for des in db.description]
 
     db_close(db)
+
+    if player:
+    	return parse_json(player,keys)
+    else:
+    	return "No results were found"
 
 def get_player_by_name(last_name,first_name=None):
 
@@ -35,16 +41,31 @@ def get_player_by_name(last_name,first_name=None):
 
     players = db.fetchall()
 
+    db_close(db)
+
     #if no players are found, return an error
     if len(players) == 0:
         return "No results were found"
     #if a single player is found, return that player
     elif len(players) == 1:
-        return players[0]
+        return parse_json(players[0])
     else:
+        for player in players:
+        	player = parse_json(player)
+
         return players
     
 
-    db_close(db)
+    
 
+
+
+def parse_json(response,columns):
+
+	response_hash = {}
+
+	for i in range(0,len(response)):
+		response_hash[columns[i]] = response[i]
+
+	return json.dumps(response_hash)
 
